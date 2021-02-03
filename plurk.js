026e496d399bf2b8feace6df4e8c20069b162f89
Chienwen Chen <github@chienwen.net>
2021-02-03 01:46:34 +0100

    add github and plurk lib

--- plurk.js
+++ plurk.js
@@ -1,3 +1,18 @@
-const github = require('./lib/github');
+const { GitHubOperator } = require('./lib/github');
 const OWNER = 'chienwen';
-const REPO = "diary";
+const REPO = "blahblah";
+
+const operator = new GitHubOperator(OWNER, REPO);
+
+operator.createOrUpdate('pizza/hut.c', "#include<stdio.h>\nint main() { return0; }\n" + Math.random())
+    .then((data) => { console.log('ok', data.status) })
+    .catch((err) => { console.log('error', err) });
+
+const plurk = require('./lib/plurk');
+
+plurk.callAPI('https://www.plurk.com/APP/Profile/getOwnProfile',
+    {"user_id": "chienwen"},
+    function(error, data, res) {
+        console.log(data);
+    }
+);
