929fd795a76aef80ad5f1ab3c682774a66f5f7c4
Chienwen Chen <github@chienwen.net>
2021-02-10 02:43:23 +0100

    fix facebook script errors

--- debug.js
+++ debug.js
@@ -13,5 +13,37 @@ github.write('good.js', sha, "hello world 4").then((data) => {
 });
 */
 
-const logger = require('./lib/logger');
-logger.info('hihi', 5566);
+//const logger = require('./lib/logger');
+//logger.info('hihi', 5566);
+
+const META_USER_AGENT_STRING = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.96 Safari/537.36';
+const META_DESC_LENGTH_MAX = 1000;
+const urlMetadata = require('url-metadata');
+const encodeUrl = require('encodeurl');
+
+const url = 'https://marketing.adecco.com.tw/edm/201901/2019SalaryGuide/AdeccoSalaryGuideTW.pdf';
+//const url = 'https://wiwi.video/';
+
+urlMetadata(encodeUrl(url), {
+    userAgent: META_USER_AGENT_STRING,
+    descriptionLength: META_DESC_LENGTH_MAX,
+    customHeaders: {
+        'cookie': 'over18=1'
+    }
+}).then((metadata) => {
+    const newRes = {
+        title: metadata.title || metadata['og:title'],
+        desc: metadata.description || metadata['og:description'],
+        iurl: metadata.image || metadata['og:image'],
+    };
+    const res = {};
+    Object.keys(newRes).forEach((k) => {
+        if (newRes[k]) {
+            res[k] = newRes[k];
+        }
+    });
+    console.log('OK');
+}).catch((err) => {
+    console.log(err);
+});
+
