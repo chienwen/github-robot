44df942af541bb4fc494d92c63cf766f338c11d8
Chienwen Chen <github@chienwen.net>
2022-04-15 20:45:59 +0800

    seperate plurk user

--- plurk.js
+++ plurk.js
@@ -1,3 +1,4 @@
+const config = require('./lib/config');
 const plurkLib = require('./lib/plurk');
 const enrichRes = require('./lib/enrichRes');
 const githubDiary = require('./lib/githubDiary');
@@ -44,7 +45,7 @@ function extractExtendedResource(url) {
     });
 }
 
-function processPlurk(plurk) {
+function processPlurk(plurk, myPlurkId) {
     processPlurkPromises.push(new Promise((resolve, reject) => {
         const dObj =  new Date(plurk.posted);
         const data = {
@@ -60,7 +61,7 @@ function processPlurk(plurk) {
         if (plurk.poll) {
             data.plat.poll = plurk.poll;
         }
-        if (plurk.limited_to && plurkLib.isLimitedToMeOnly(plurk.limited_to)) {
+        if (plurk.limited_to && plurkLib.isLimitedToMeOnly(plurk.limited_to, myPlurkId)) {
             data.plat.priv = true;
         }
         const m = data.ct.replace(/\n/g, ' ').trim().match(/^(.*)(https?:\/\/[^ ]+)$/);
@@ -89,6 +90,10 @@ function backupPlurk(dateTimeFrom) {
     logger.info('backup start at', dateTimeFrom);
     plurkLib.callAPI('/APP/Timeline/getPlurks',
         {
+            token: config('PLURK_SMULLERS_OAUTH_ACCESS_TOKEN'),
+            secret: config('PLURK_SMULLERS_OAUTH_ACCESS_TOKEN_SECRET'),
+        },
+        {
             limit: FETCH_BATCH_SIZE,
             filter: 'my',
             offset: dateTimeFrom.toISOString()
@@ -98,7 +103,9 @@ function backupPlurk(dateTimeFrom) {
             logger.info('new fetched', plurks.length, ', already fetched', fetchedCount, '/', FETCH_COUNT_LIMIT);
             if (plurks.length > 0) {
                 fetchedCount += plurks.length;
-                plurks.forEach(processPlurk);
+                plurks.forEach((plurk) => {
+                    processPlurk(plurk, config('PLURK_SMULLERS_USER_ID'));
+                });
                 if (fetchedCount < FETCH_COUNT_LIMIT) {
                     const nextDateTime = new Date(plurks[plurks.length - 1].posted);
                     backupPlurk(nextDateTime);
@@ -115,6 +122,10 @@ function backupPlurk(dateTimeFrom) {
 
 function backupPlurkDone(isEndByCountLimit) {
     logger.info('Fetching done', isEndByCountLimit);
+    
+    // Debugging, don't push
+    //Promise.all(processPlurkPromises).then((items) => { console.log(require('util').inspect(items, {showHidden: false, depth: null}))}); return;
+
     Promise.all(processPlurkPromises).then(githubDiary.publishDiaryItems).then(() => {
         logger.info('All done.');
     });
